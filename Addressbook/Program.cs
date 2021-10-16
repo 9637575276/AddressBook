a25c45c7f76434b7ce0f5253c5fb23b977d8fa06
Shubham Mule <shubhambmule@gmail.com>
2021-10-16 22:06:18 +0530

    Added :Read person details from user

--- Addressbook/Program.cs
+++ Addressbook/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 
-namespace Addressbook
+namespace AddressBook
 {
-    class Addressbook
+    class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook");
-        }
+            Console.WriteLine("Hello Welcome To AddressBook Management");
+            //String fname, lname, address, city, state, zip, phone;
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Enter phone number");
+            string phoneNumber = Console.ReadLine();
 
+        }
     }
 }
